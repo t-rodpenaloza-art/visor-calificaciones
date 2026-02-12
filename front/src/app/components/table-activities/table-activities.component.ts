387420ec3828c652d4d0deeb58393ae5c4607045
t-rodpenaloza-art <t-rodpenaloza@itesm.mx>
2026-02-12 16:21:52 -0600

    feat: add table-courses, integral-dashboard and rating-details components

---
+++ front/src/app/components/table-activities/table-activities.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-activities',
+  imports: [],
+  templateUrl: './table-activities.component.html',
+  styleUrl: './table-activities.component.scss',
+})
+export class TableActivitiesComponent {
+
+}
